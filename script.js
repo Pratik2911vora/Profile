a0afea9c362cabf78a3e1f6132eaa4895c0636fb
Pratik2911vora <136695090+Pratik2911vora@users.noreply.github.com>
2023-07-29 15:24:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// eader toggle
+let MenuBtn = document.getElementById("MenuBtn");
+MenuBtn.addEventListener("click", function (e) {
+  document.querySelector("body").classList.toggle("mobile-nav-active");
+  this.classList.toggle("fa-xmark");
+});
+
+let typed = new Typed(" .auto-input", {
+  strings: ["MERN Stack Developer!", "Cloud !", "Engineer!"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 2000,
+  loop: true,
+});
